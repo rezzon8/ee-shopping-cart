59c911d99d9fe4f2d1ca39d97aefb968e5d6b98b
Clinton.Botha <clinton.botha@coveainsurance.co.uk>
2023-11-09 13:06:05 +0200

    typescript types

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+interface shopProps {
+  salesTaxRate: number
+}
+
+export interface product {
+  name: string
+  price: number
+}
+
+export type cart = product[]
+
+export type products = product[]
