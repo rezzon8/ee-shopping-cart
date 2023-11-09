d2ee920e161ac2eb63e9624afdecad31fdf51f62
Clinton.Botha <clinton.botha@coveainsurance.co.uk>
2023-11-09 13:06:43 +0200

    shopping cart app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useState } from 'react'
+import { product, shopProps } from './types'
+import { getTotalCost, formatCurrency, addSalesTax } from './helpers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const doveSoap: product = {
+  name: 'Dove Soap',
+  price: 39.99
 }
 
-export default App;
+const axeDeo: product = {
+  name: 'Axe Deo',
+  price: 99.99
+}
+
+const App = ({ salesTaxRate }: shopProps) => {
+  const [cart, setCart] = useState<product[]>([])
+
+  return (
+    <div>
+      <button
+        data-testid="dove-soap"
+        onClick={() => setCart(prevCart => [...prevCart, doveSoap])}
+      >
+        {doveSoap.name} - {doveSoap.price}
+      </button>
+      <button
+        data-testid="axe-deo"
+        onClick={() => setCart(prevCart => [...prevCart, axeDeo])}
+      >
+        {axeDeo.name} - {axeDeo.price}
+      </button>
+
+      <h4>Cart</h4>
+
+      {cart.length > 0 ? (
+        cart.map((product, index) => {
+          return (
+            <div key={index}>
+              <h2>
+                {product.name} - {formatCurrency(product.price)}
+              </h2>
+            </div>
+          )
+        })
+      ) : (
+        <h2>No products to display</h2>
+      )}
+
+      <div>
+        Total: {formatCurrency(addSalesTax(getTotalCost(cart), salesTaxRate))}
+      </div>
+    </div>
+  )
+}
+
+export default App
