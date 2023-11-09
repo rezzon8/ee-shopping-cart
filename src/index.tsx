07aa44b33813c9c62461df31da298f3eb5344f6a
Clinton.Botha <clinton.botha@coveainsurance.co.uk>
2023-11-09 16:04:02 +0200

    add csstools/normalize.css dependency and import into index.js

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import '@csstools/normalize.css'
 import { SALES_TAX_RATE } from './constants'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
